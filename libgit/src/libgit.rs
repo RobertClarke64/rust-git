ea6445dc27c8b68345dcba6f16917e7b1193e161
Robert Clarke <robert.clarke1@bp.com>
2024-01-17 20:07:39 +0000

    get current dir

--- libgit/src/libgit.rs
+++ libgit/src/libgit.rs
@@ -1,12 +1,12 @@
 mod repository;
 
+use std::env;
 use repository::GitRepository;
 
 use std::path::PathBuf;
 
 pub fn cmd_init() {
-    println!("libgit::cmd_init() success");
-    let path = PathBuf::from(".");
+    let path = env::current_dir().unwrap();
     let repo = GitRepository::new(path, false);
 
     dbg!(repo);
